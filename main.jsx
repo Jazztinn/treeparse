d88501a76b23771d77551acd0e25a782087e41d0
Kira-Ace <legaspijazztinnkyle@gmail.com>
2026-04-29 15:16:36 +0800

    added npm

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import MainPage from './mainpage.jsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<MainPage />);
