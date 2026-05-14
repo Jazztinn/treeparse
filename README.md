63715d03bb2adfc215dc9b1e02bf0f2171274f96
Kira-Ace <legaspijazztinnkyle@gmail.com>
2026-05-14 17:27:42 +0800

    cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# treeparse
-for easy-visualization of any sentence's grammatical structure as an interactive syntax tree.
--- mainpage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import TreeParse from './pages/TreeParse';
-
-export default function MainPage() {
-  return <TreeParse />;
-}
